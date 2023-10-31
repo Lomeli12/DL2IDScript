14e1e85473173eafadf81ee6c99a6b4c9860fa8b
Anthony Lomeli <anthonylomeli11@gmail.com>
2023-10-31 13:34:06 -0700

    Slight tweaks for if user decides not to use data2.pak.
    First change, give prompts to tell the user what number it has selected.
    
    Second, starting count on data3.pak instead of 2. That way, it doesn't select 2 if the user said no on the previous prompt.

--- DL2IDScript.py
+++ DL2IDScript.py
@@ -103,11 +103,14 @@ def create_or_replace_mod():
     replace_pak = input("Replace data2.pak, if it exists? (Y/N): ")
     mod_data_pak = os.path.join(data_0_parent_dir, BASE_DATA_NAME + "2" + DATA_PAK_EXT)
     if replace_pak.lower() == "n":
+        print("-> Looking for unused data pak number")
         mod_data_pak = find_suitable_pak_name(2)
+        print("-> Using " + os.path.basename(mod_data_pak))
     elif replace_pak.lower() != "y":
         create_or_replace_mod()
 
-    package_mod(mod_data_pak,)
+    package_mod(mod_data_pak)
+
 
 def package_mod(data_pak):
     print("-> Creating " + data_pak + "!")
